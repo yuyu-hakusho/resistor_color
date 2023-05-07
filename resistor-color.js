2d2b5401d816a9e5b03581053a65ef739cde3f09
Jennifer-Tsang_lmig <jennifer.tsang@libertymutual.com>
2023-05-07 13:27:02 -0400

    add files to the repo

--- resistor-color.js
+++ resistor-color.js
@@ -1,3 +1,19 @@
-export function colorCode() {}
 
-export const COLORS = [];
+const colorsMap = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9 
+};
+
+export function colorCode(color) {
+    return colorsMap[color];
+}
+
+export const COLORS = ["black", "brown", "red","orange","yellow","green","blue","violet","grey","white"];
